8fdeca0d2327f9a5ba9886d01748c2f45315af19
crazyvibes <bkm123r@gmail.com>
2021-04-16 00:38:11 +0530

    first test case implemented for liveData

--- app/src/main/java/in/bk/simpleunittesting/Repository/Repository.java
+++ app/src/main/java/in/bk/simpleunittesting/Repository/Repository.java
@@ -1,9 +1,23 @@
 package in.bk.simpleunittesting.Repository;
 
-public class Repository {
+import android.util.Log;
 
+import androidx.lifecycle.MutableLiveData;
+
+public class Repository {
+    public MutableLiveData<String> liveData=new MutableLiveData<>();
     public String getData()
     {
         return "Hi this is BK.";
+    }
+
+    public void getData2(String name,String pass)
+    {
+        Log.d("TAG", "Name is "+name+". Password is "+pass);
+    }
+
+    public MutableLiveData<String> getLiveData(String name, String password) {
+        liveData.setValue("name is "+name+" password is "+password);
+        return liveData;
     }
 }
--- app/src/test/java/in/bk/simpleunittesting/Repository/RepositoryTest.java
+++ app/src/test/java/in/bk/simpleunittesting/Repository/RepositoryTest.java
@@ -1,7 +1,11 @@
 package in.bk.simpleunittesting.Repository;
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import androidx.lifecycle.MutableLiveData;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -11,14 +15,19 @@ import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
 
 public class RepositoryTest {
 
+
     @Spy
     Repository repository;
+
+    @Rule
+    public InstantTaskExecutorRule instantTaskExecutorRule=new InstantTaskExecutorRule();//help to run synchronously
 
     @Before
     public void setUp() throws Exception {
@@ -29,11 +38,32 @@ public class RepositoryTest {
     @Test
     public void getData(){
 
+        //with fake object
+//        MutableLiveData<String> dummyLiveData=new MutableLiveData<>();
+//        dummyLiveData.setValue("this is login test");
+//        when(repository.getLiveData("Bk","123456")).thenReturn(dummyLiveData);//whenever we work with thenReturn we have to pass dummy data
+//        System.out.println(dummyLiveData.getValue());
+
+        //with real object
+        doReturn(repository.getLiveData("Bkm","123")).when(repository).getLiveData("Bkm","123");
+        System.out.println(repository.liveData.getValue());
+
+
+
        // when(repository.getData()).thenReturn("Hi this  is test.");//this is for mock, generating dummy return type
-        doReturn(repository.getData()).when(repository).getData();//this is for spy, getting actual return type
-        System.out.println(repository.getData());
+      //  doReturn(repository.getData()).when(repository).getData();//this is for spy, getting actual return type
+       // System.out.println(repository.getData());
     }
 
+
+
+    @Test
+    public void getData2(){
+        //for void return type
+      //  doNothing().when(repository).getData2("Bk","123456");
+
+
+    }
 
     @After
     public void tearDown() throws Exception {
