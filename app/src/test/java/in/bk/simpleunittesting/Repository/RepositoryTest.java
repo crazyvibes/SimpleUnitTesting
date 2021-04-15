516772a2cdbe7e92c56c6e8fcb8d89a62550fa56
crazyvibes <bkm123r@gmail.com>
2021-04-16 00:13:00 +0530

    first test case implemented for spy

--- app/src/test/java/in/bk/simpleunittesting/Repository/RepositoryTest.java
+++ app/src/test/java/in/bk/simpleunittesting/Repository/RepositoryTest.java
@@ -7,26 +7,30 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
 
 public class RepositoryTest {
 
-    @Mock
+    @Spy
     Repository repository;
 
     @Before
     public void setUp() throws Exception {
-        repository=Mockito.mock(Repository.class);
+        //repository=Mockito.mock(Repository.class); //initializing for mock
+        repository=Mockito.spy(Repository.class);//initializing for spy
     }
 
     @Test
     public void getData(){
 
-        when(repository.getData()).thenReturn("Hi this  is test.");
+       // when(repository.getData()).thenReturn("Hi this  is test.");//this is for mock, generating dummy return type
+        doReturn(repository.getData()).when(repository).getData();//this is for spy, getting actual return type
         System.out.println(repository.getData());
     }
 
